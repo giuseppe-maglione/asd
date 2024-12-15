92dd129fe8711a178b9b79324b732c2f72f5c3bb
Giuseppe <148074888+giuseppe-maglione@users.noreply.github.com>
2024-12-15 12:20:59 +0100

    Update problema1.py

--- Homework 2/problema1.py
+++ Homework 2/problema1.py
@@ -3,6 +3,8 @@ def largest_sum_subarray(arr: list[int]) -> int:
     n = len(arr)
     if n == 0:
         return 0
+    if n == 1:
+        return arr[0]
 
     max_sums = {}
 
@@ -21,3 +23,49 @@ def largest_sum_subarray(arr: list[int]) -> int:
         get_max_sum(i)
 
     return max( max_sums.values() )
+
+def test_func(array, result): # funzione per il testing
+    sum = largest_sum_subarray(array)
+    if sum == result:
+        return True, sum
+    else:
+        return False, sum   
+
+if __name__ == '__main__':
+
+    array_test0 = [-1, -3, 4, 2]
+    array_test1 = [-1, 2, -5, 7]
+    array_test2 = []
+    array_test3 = [-10, ]
+
+    print('\n')
+
+    print('[TEST CASE 0] Testing array: ' + str(array_test0))
+    success, sum = test_func(array_test0, 6)
+    if success:
+        print('Result: ' + str(sum) + ' -> SUCCESS.')
+    else:
+        print('Result: ' + str(sum) + ' -> FAILURE.')
+
+    print('\n[TEST CASE 1] Testing array: ' + str(array_test1))
+    success, sum = test_func(array_test1, 7)
+    if success:
+        print('Result: ' + str(sum) + ' -> SUCCESS.')
+    else:
+        print('Result: ' + str(sum) + ' -> FAILURE.')
+
+    print('\n[TEST CASE 2] Testing array: ' + str(array_test2))
+    success, sum = test_func(array_test2, 0)
+    if success:
+        print('Result: ' + str(sum) + ' -> SUCCESS.')
+    else:
+        print('Result: ' + str(sum) + ' -> FAILURE.')
+
+    print('\n[TEST CASE 3] Testing array: ' + str(array_test3))
+    success, sum = test_func(array_test3, -10)
+    if success:
+        print('Result: ' + str(sum) + ' -> SUCCESS.')
+    else:
+        print('Result: ' + str(sum) + ' -> FAILURE.')
+
+    print('\n')    
